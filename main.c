8821998f7fa58aa08afe33cb9a58131e90a8e542
Ivan <vana.shimko@gmail.com>
2015-11-18 21:53:43 +0300

    Moving rectangle

--- main.c
+++ main.c
@@ -1,16 +1,170 @@
 #include <graphics.h> 
+#include <X11/Xlib.h>
+
+#define MAX_WIDTH 320
+#define MAX_HEIGHT 200
+#define DELTA 10
+
+typedef struct{
+	int x;
+	int y;
+} TPoint;
+
+typedef struct{
+	TPoint leftUp;
+	TPoint rightDown;
+} TRectangle;
+
+void DrawRectangle(const TRectangle *const rect){
+	rectangle(rect->leftUp.x, rect->leftUp.y, rect->rightDown.x, rect->rightDown.y);
+}
+
+char IncHeight(TRectangle *const rect) {
+	if (rect->leftUp.y - DELTA  > 0){
+		rect->leftUp.y -= DELTA;
+		return 1;
+	} else {
+		return 0;
+	}
+}
+
+char IncWidth(TRectangle *const rect) {
+	if (rect->rightDown.x + DELTA < MAX_WIDTH) {
+		rect->rightDown.x += DELTA;
+		return 1;
+	} else {
+		return 0;
+	}
+}
+
+char DecHeight(TRectangle *const rect) {
+	if (rect->leftUp.y + DELTA < MAX_HEIGHT) {
+		rect->leftUp.y += DELTA;
+		return 1;
+	} else {
+		return 0;
+	}
+}
+
+char DecWidth(TRectangle *const rect) {
+	if (rect->rightDown.x - DELTA > 0) {
+		rect->rightDown.x -= DELTA;
+		return 1;
+	} else {
+		return 0;
+	}
+}
+
+char MoveLeft(TRectangle *const rect) {
+	if (rect->leftUp.x - DELTA > 0) {
+		rect->leftUp.x -= DELTA;
+		rect->rightDown.x -= DELTA;
+		return 1;
+	} else {
+		return 0;
+	}
+}
+
+char MoveRight(TRectangle *const rect) {
+	if (rect->rightDown.x + DELTA < MAX_WIDTH) {
+		rect->rightDown.x += DELTA;
+		rect->leftUp.x += DELTA;
+		return 1;
+	} else {
+		return 0;
+	}
+}
+
+char MoveUp(TRectangle *const rect) {
+	if (rect->leftUp.y - DELTA > 0) {
+		rect->leftUp.y -= DELTA;
+		rect->rightDown.y -= DELTA;
+		return 1;
+	} else {
+		return 0;
+	}
+}
+
+char MoveDown(TRectangle *const rect) {
+	if (rect->leftUp.y + DELTA < MAX_HEIGHT) {
+		rect->leftUp.y += DELTA;
+		rect->rightDown.y += DELTA;
+		return 1;
+	} else {
+		return 0;
+	}
+}
+
+void Refresh(TRectangle *const rect) {
+	cleardevice();
+	DrawRectangle(rect);
+}
+
 int main()
 {
-   int gd = DETECT,gm,left=100,top=100,right=200,bottom=200,x= 300,y=150,radius=50;
-   initgraph(&gd,&gm,NULL);
-   rectangle(left, top, right, bottom);
-   circle(x, y, radius);
-   bar(left + 300, top, right + 300, bottom);
-   line(left - 10, top + 150, left + 410, top + 150);
-   ellipse(x, y + 200, 0, 360, 100, 50);
-   outtextxy(left + 100, top + 325, "C Graphics Program");
+	
+	XInitThreads();
+	int gd = DETECT,gm;	
+	initgraph(&gd,&gm,"C:\\TC\\BGI");
 
-   delay(5000);
-   closegraph();
-   return 0;
+	
+	char c;
+	TRectangle myRectangle;
+	myRectangle.leftUp.x = 1;
+	myRectangle.leftUp.y = 1;
+	myRectangle.rightDown.x = 51;
+	myRectangle.rightDown.y = 101;
+	
+	Refresh(&myRectangle);
+  
+	do {		
+		c = getch();
+		switch(c) {
+			case 'u':
+				if (IncHeight(&myRectangle)) {
+					//printf("ddd");
+					Refresh(&myRectangle);					
+				}
+				break;
+			case 'j':
+				if (DecHeight(&myRectangle)) {
+					//printf("ddd");
+					Refresh(&myRectangle);
+				}
+				break;
+			case 'w':
+				if (MoveUp(&myRectangle)) {
+					Refresh(&myRectangle);
+				}
+				break;
+			case 's':
+				if (MoveDown(&myRectangle)) {
+					Refresh(&myRectangle);
+				}
+				break;
+			case 'd':
+				if (MoveRight(&myRectangle)) {
+					Refresh(&myRectangle);
+				}
+				break;
+			case 'a':
+				if (MoveLeft(&myRectangle)) {
+					Refresh(&myRectangle);
+				}
+				break;
+			case 'k':
+				if (IncWidth(&myRectangle)) {
+					Refresh(&myRectangle);
+				}
+				break;
+			case 'h':
+				if (DecWidth(&myRectangle)) {
+					Refresh(&myRectangle);
+				}
+				break;
+		}	
+	}while (c != 32);
+	//delay(5000);
+	closegraph();
+	return 0;
 }
